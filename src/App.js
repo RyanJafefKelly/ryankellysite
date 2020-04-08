211d3424cc2e26bbebd1ab028aed3cf80380748a
RyanJafefKelly <ryan@kiiii.com>
2020-04-08 17:55:12 +1000

    add initial routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { HashRouter, BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './routes/HomePage';
+import Projects from './routes/Projects';
 
 
 function App() {
   return (
-    <div>
-      ryankellysite
-    </div>
+    <HashRouter>
+      <React.Suspense>
+        <Switch>
+          <Route exact path="/" name="Homepage" render={HomePage} />
+          <Route path="/projects" name="Projects" render={Projects} />
+        </Switch>
+      </React.Suspense>
+    </HashRouter>
   );
 }
 
