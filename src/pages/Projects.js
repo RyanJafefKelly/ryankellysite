c25360185170f1ef8a14f794db1112fdfa683150
RyanJafefKelly <ryan@kiiii.com>
2020-04-10 17:25:05 +1000

    Add ProjectItem to project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import ProjectItem from '../components/ProjectItem';
 
 const Projects = () => {
-    return(<div>
-        Projects
-    </div>);
+
+    const projects = [
+        {id:0, name:'test0', description:'testdescription', link:'www.google.com'},
+        {id:1, name:'test1', description:'testdescriptionv2', link:'www.google.com'}
+    ];
+
+    return(<React.Fragment>
+        <Typography style={{      
+            paddingLeft:20,  
+            fontFamily: 'Arial',
+            fontSize: 40,
+        paddingTop: 5}}
+    > 
+    Projects</Typography>
+    {projects.map((project, idx) => {
+        return (
+            <ProjectItem key={projects[idx].id} props={projects[idx]} />
+        );
+    })}
+    </React.Fragment>);
 };
 
 export default Projects;
